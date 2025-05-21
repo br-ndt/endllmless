461f7b616b8bf881f08d4111711475301203866b
VoX <v@bitvox.me>
2025-05-20 20:49:34 -0400

    more creative again

--- server/routes/wordCombine.js
+++ server/routes/wordCombine.js
@@ -34,7 +34,6 @@ router.get('/', async (req, res, next) => {
         "content": `User: Play a word game with me.\n      \n       Here are the rules of the game:\n       - I will give you two words, and you will respond with a conceptual combination of the two.\n    - The two words for this round are: \"${wordOne}\" + \"${wordTwo}\" = ?\n       - The word must be a noun.\n       - The word should not simply be a combination of the two words, unless it is a commonly used word.\n       - Do not elaborate, use single word responses only.\n       - Try not to respond with either input word.\n             - Please prefer very commonplace and physical nouns where possible\n       - Choose the most commonsense interpretation for the combination.\n      \n       Some example rounds:\n       - \"Fire\" + \"Ice\" = \"Water\"\n       - \"Water\" + \"Fire\" = \"Steam\"\n       - \"Water\" + \"Earth\" = \"Mud\"\n       - \"Fire\" + \"Fire\" = \"Volcano\"\n       - \"Steam\" + \"Cloud\" = \"Rain\"\n       - \"Death\" + \"Human\" = \"Corpse\"\n       - \"Tomato\" + \"Bread\" = \"Pizza\"\n       - \"Bread\" + \"Fire\" = \"Toast\"\n       - \"Wind\" + \"Fire\" = \"Smoke\"\n       - \"Smoke\" + \"Smoke\" = \"Cloud\"\n      \n       Again, the two words for this round are: \"${wordOne}\" + \"${wordTwo}\" = ?`
       }
     ],
-    top_p: 0.1,
     max_tokens: 256,
   });
 
@@ -48,7 +47,6 @@ router.get('/', async (req, res, next) => {
         "content": `Respond with the emoji that most-literally represents the meaning of the word "${newWord}". Please respond only with an emoji, do not elaborate.`
       }
     ],
-    top_p: 0.2,
     max_tokens: 256,
   });
   const newEmoji = emojiResponse.choices[0].message.content.trim().replaceAll("\"", "");
